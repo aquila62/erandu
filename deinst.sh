e250bd42a545f4e8a5b9e8c3a901e24f7db079a1
aquila62 <aquila62@users.noreply.github.com>
2016-09-28 09:10:48 -0500

    Added compiles for new tests.

--- deinst.sh
+++ deinst.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 make -f binom.mak clean
+make -f captcha.mak clean
 make -f eranduchi.mak clean
 make -f erandudots.mak clean
+make -f eranduencr.mak clean
 make -f erandugen.mak clean
 make -f erandunorm.mak clean
 make -f erandupat.mak clean
@@ -9,12 +11,15 @@ make -f erandupbm.mak clean
 make -f eranduppm.mak clean
 make -f eranduraw.mak clean
 make -f erandutim.mak clean
+make -f genalfa.mak clean
+make -f gendna.mak clean
 make -f mcbell.mak clean
 make -f mccos.mak clean
 make -f mcparab.mak clean
 make -f mcpi.mak clean
 make -f mcsin.mak clean
 make -f mcsph.mak clean
+make -f pair.mak clean
 make -f poischi.mak clean
 make -f pwrball.mak clean
 make -f ranwlk.mak clean
@@ -22,3 +27,5 @@ make -f tsterandu.mak clean
 make -f uintchi.mak clean
 make -f z80rnd.mak clean
 make -f randu.mak clean         # uses z80asm assembler
+rm -f pic.png
+rm -f rwxf
--- inst.sh
+++ inst.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 make -f binom.mak
+make -f captcha.mak
 make -f eranduchi.mak
 make -f erandudots.mak
+make -f eranduencr.mak
 make -f erandugen.mak
 make -f erandunorm.mak
 make -f erandupat.mak
@@ -9,12 +11,15 @@ make -f erandupbm.mak
 make -f eranduppm.mak
 make -f eranduraw.mak
 make -f erandutim.mak
+make -f genalfa.mak
+make -f gendna.mak
 make -f mcbell.mak
 make -f mccos.mak
 make -f mcparab.mak
 make -f mcpi.mak
 make -f mcsin.mak
 make -f mcsph.mak
+make -f pair.mak
 make -f poischi.mak
 make -f pwrball.mak
 make -f ranwlk.mak
